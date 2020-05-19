6cf28d5aa65fbfd46d704f31c6aa50a99dadf8a7
Johannes Stephan Przymusinski <johannes.przymusinski@jop-software.de>
2020-05-19 10:46:21 +0200

    create files

---
+++ head.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8">
+<meta name="author" content="Johannes Przymusinski">
+<meta name="description" content="Startseite für den HTML & CSS Kurs">
+<meta name="keywords" content="Startseite, Website, Kurs">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="https://jop-software.de/wp-content/uploads/Logo-Website.png">
---
+++ schrift/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "../head.php"?>
+
+    <!-- HTML-Title -->
+    <title>Schrift</title>
+    <!-- Link to Stylesheet -->
+    <link href="https://fonts.googleapis.com/css2?family=Vollkorn&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+</body>
+</html>
